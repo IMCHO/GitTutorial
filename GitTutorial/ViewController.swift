2f328973b83e86977af618743e292ed129c85774
IMCHO <dlsxor21c@naver.com>
2019-02-21 15:15:50 +0900

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by In Taek Cho on 21/02/2019.
+//  Copyright © 2019 IMCHO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
